87b34defd8d9ce423bc375a61765b98abbcbc770
Luke Gosnell <luke_gosnell@ranken.edu>
2019-11-19 13:43:20 -0600

    11/19/19 commit

---
+++ 10_12_19_SalesOrderSolution/SalesOrdersProject/Controllers/RegisterController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SalesOrdersProject.Models;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+
+
+namespace SalesOrdersProject.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private SalesOrdersDatabase2Entities db = new SalesOrdersDatabase2Entities();
+
+        [HttpGet]
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View(new Customer());
+        }
+
+        [HttpPost]
+        public RedirectToRouteResult Register(Customer customer)
+        {
+            //customer.CustomerPassword = SHA256.Encode(customer.CustomerPassword);
+
+            db.Customers.Add(customer);
+            db.SaveChanges();
+
+            //try
+            //{
+            //    db.SaveChanges();
+            //}
+
+            //catch (DbEntityValidationException dbEx)
+            //{
+            //    foreach (var validationErrors in dbEx.EntityValidationErrors)
+            //    {
+            //        foreach (var validationError in validationErrors.ValidationErrors)
+            //        {
+            //            Trace.TraceInformation("Property: {0} Error: {1}",
+            //                                    validationError.PropertyName,
+            //                                    validationError.ErrorMessage);
+            //        }
+            //    }
+            //}
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ 10_12_19_SalesOrderSolution/SalesOrdersProject/Models/ShoppingCartModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesOrdersProject.Models
+{
+    public class ShoppingCartModel
+    {
+        private List<ShoppingCartItemModel> items = new List<ShoppingCartItemModel>();
+
+        public IEnumerable<ShoppingCartItemModel> Items
+        {
+            get { return items; }
+        }
+
+        public void AddItem(Product product, int quantity)
+        {
+            ShoppingCartItemModel item = items.SingleOrDefault(
+                                         p => p.Product.ProductID ==
+                                                product.ProductID);
+
+            //  Item does not currently exist in shopping cart
+            if (item == null)
+            {
+                items.Add(new ShoppingCartItemModel
+                {
+                    Product  = product,
+                    Quantity = quantity
+                });
+            }
+            //  Otherwise, product exists in current shopping cart
+            else
+            {
+                item.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(int productID)
+        {
+            items.RemoveAll(p => p.Product.ProductID == productID);
+        }
+
+        public decimal GetCartTotal()
+        {
+            return (decimal)items.Sum(s => s.Product.ProductPrice * s.Quantity);
+        }
+
+        public void Clear()
+        {
+            items.Clear();
+        }
+
+        public ShippingInfo ShippingInfo { get; set; }
+        public BillingInfo BillingInfo { get; set; }
+    }
+}
